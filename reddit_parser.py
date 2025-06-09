8da3202727b7caa014b9b8133f8a07e3370e0059
shoaibwaqar665 <shoaibwaqar@weteck.co>
2025-06-09 21:28:14 +0500

    fix the reddit

--- reddit_parser.py
+++ reddit_parser.py
@@ -2,45 +2,63 @@ import requests
 import json
 from dbOperations import store_data_in_db
 
-# Example: fetch Reddit data from a subreddit
-#  remove last '/' from the url using string manipulation
-url = "https://www.reddit.com/r/pennystocks/comments/1judcea/ive_developed_a_new_watchlist_of_stocks_to_keep/"
-url = url[:-1]
-url = url + ".json"
-print(url)
-headers = {'User-Agent': 'Mozilla/5.0'}
+def extract_data():
+    
+    # Custom headers to avoid 429 Too Many Requests or 403 Forbidden
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    }
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
 # Custom headers to avoid 429 Too Many Requests or 403 Forbidden
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) RedditScraper/1.0"
-}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) RedditScraper/1.0"
+    }
 
-response = requests.get(url, headers=headers)
+    urls = [
+        "https://www.reddit.com/r/pennystocks/comments/1jewiqr/a_unique_beverage_company_caught_my_eye_and/",
+        "https://www.reddit.com/r/pennystocks/comments/1jgg8bo/why_im_high_on_beverages_right_now_and_why_you/",
+        "https://www.reddit.com/r/pennystocks/comments/1jpo611/hear_me_out_theres_some_recent_catalysts_on_my/",
+        "https://www.reddit.com/r/pennystocks/comments/1jqhr68/after_a_fundamental_breakdown_yesterday_heres_how/",
+        "https://www.reddit.com/r/pennystocks/comments/1jtkvwa/weekly_watchlist_shot_heating_up_while_prop/",
+        "https://www.reddit.com/r/pennystocks/comments/1jvxtur/markets_pop_on_surprise_pause_heres_what_im/",
 
-# Parse the response and extract the data
-filtered_posts = []
+    ]
+    all_results = []
+    for url in urls:
+        url = url[:-1]
+        url = url + ".json"
+        print(url)
+        response = requests.get(url, headers=headers)
+        # Parse the response and extract the data
 
-if response.status_code == 200:
-    listings = response.json()  # This is a list
-    for listing in listings:
-        for post in listing.get("data", {}).get("children", []):
-            post_data = post.get("data", {})
-            filtered_post = {
-                "title": post_data.get("title", ""),
-                "description": post_data.get("selftext", ""),
-                "likes": post_data.get("ups", 0),
-                "comments": post_data.get("num_comments", 0),
-                "platform": "Reddit",
-                "url": url
-            }
-            filtered_posts.append(filtered_post)
-else:
-    print("Failed to fetch data. Status code:", response.status_code)
-# Print extracted posts
-print(filtered_posts[0])
 
-# Write the filtered posts to a JSON file
-with open('reddit_data.json', 'w') as f:
-    json.dump(filtered_posts[0], f, indent=4, ensure_ascii=False)
+        if response.status_code == 200:
+            listings = response.json()  # This is a list
+            print(listings)
+            for listing in listings:
+                for post in listing.get("data", {}).get("children", []):
+                    post_data = post.get("data", {})
+                    filtered_post = {
+                        "title": post_data.get("title", ""),
+                        "description": post_data.get("selftext", ""),
+                        "likes": post_data.get("ups", 0),
+                        "comments": post_data.get("num_comments", 0),
+                        "platform": "Reddit",
+                        "url": url
+                    }
+                    if filtered_post["description"] != "":
+                        all_results.append(filtered_post)
+                    
+        else:
+            print("Failed to fetch data. Status code:", response.status_code)
 
-store_data_in_db(filtered_posts[0])
+        # write the result to a file
+        with open('reddit_data.json', 'w', encoding='utf-8') as file:
+            json.dump(all_results, file, indent=4, ensure_ascii=False)
+
+    for result in all_results:
+        store_data_in_db(result)
+
+if __name__ == "__main__":
+    extract_data()
